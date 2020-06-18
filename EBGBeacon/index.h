1aa38e6ee5b7b8e4f38ff92eb2068a7c2c77d6ff
Octium-II <66820996+Octium-II@users.noreply.github.com>
2020-06-18 19:26:46 +1000

    html for landing page

---
+++ EBGBeacon/index.h
@@ -0,0 +1,73 @@
+const char index_page[] = R"=====(
+<!DOCTYPE html>
+<html>
+<style>
+body {
+  background-color: linen;
+  width:90%;
+  font-size: 300%;
+  margin-right: 5%;
+  margin-left: 5%;
+}
+
+div.wrap {
+  text-align: justify;
+}
+
+div.center {
+  text-align: center;
+}
+
+h1 {
+  color: maroon;
+  margin-left: 40px;
+  text-align: center;
+} 
+</style>
+<body>
+<h1>#ExposeBillGates</h1>
+<div class="wrap">
+<p>In the first few months of 2020, billionaire Bill Gates has been promoted as the savior to the COVID-19 pandemic. 
+Although he is lauded as a hero Gates has been a force of disruption and shaky science. 
+The truth is Bill Gates hides a darker agenda.</P>
+
+<p>Gates has made numerous media appearances calling for extended lock downs,
+contact tracing surveillance, digital certificates to travel and work, 
+and announced his intention to vaccinate 7 billion people. 
+Gates' family also has ties to discredited eugenics science and believes billionaires like him should help reduce the world's population.</p>
+
+<p>It's time to #ExposeBillGates and expose his agenda. Together we can shine the light on Gates' true intentions and change the course of humanity.</p>
+
+<p>We encourage everyone interested to organize and host documentary screenings, share articles, 
+pass out flyers, drop banners, and share on social media using the hashtag #ExposeBillGates. 
+We encourage everyone to share accurate and credible information about Gates' goals.</p>
+</div>
+<div class="center">
+<p>Find more out at</p>
+<p><a href="https://corbettreport.com/gates">corbettreport.com</a></p>
+<br>
+</div>
+<h1>Freedom Cells</h1>
+<div class="wrap">
+<p>Freedom Cells are peer to peer groups organizing themselves in a decentralized manner with the collective goal of asserting 
+the sovereignty of group members through peaceful resistance and the creation of alternative institutions. </p>
+</div>
+<div class="center">
+<p>Find more out at</p>
+<p><a href="https://freedomcells.org/">freedomcells.org</a></p>
+<br>
+</div>
+<br>
+<h1>Wifi Beacon</h1>
+<div class="wrap">
+<p>The page your are viewing now is being transmitted from a small Wi-fi Beacon that is near by. 
+These beacons are designed to spread news and information 
+that is being censored by government and big tech corporations that don't have your interests at heart.</p> 
+<p>If you click on the links provided you may receive a warning that you cannot connect - 
+that is because this beacon is not connected to the Internet. 
+Please Bookmark or record the links and visit them later when your device is connected to the Internet.</p>
+<p>If you would like to learn more or find out how to set up beacons like this in your own area, please
+visit the freedomcells.org site above and join the "Wifi Beacon" cell.</p> 
+</body>
+</html>
+)=====";
